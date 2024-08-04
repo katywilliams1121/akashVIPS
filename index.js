ba2c1d7b0bc9ce1bb4bc72e72a105646d280ed86
katywilliams1121 <151863894+katywilliams1121@users.noreply.github.com>
2024-08-04 18:24:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.post('/run-command', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(Server is running on port ${port});
+    console.log(`Server is running on port ${port}`);
 });
